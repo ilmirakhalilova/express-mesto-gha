44e487dcc050155fee5cee27e7a3c7493756502f
ilmira khalilova <mirakhaleesi@yandex.ru>
2023-10-03 18:28:34 +0300

    ПР14 v6

--- routes/card.js
+++ routes/card.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();// импортируем модель
+const { celebrate, Joi } = require('celebrate');
 const {
   findAllCards,
   createCard,
@@ -8,9 +9,30 @@ const {
 } = require('../controllers/cards');
 
 router.get('/', findAllCards);
-router.post('/', createCard);
-router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', likeCard); // поставить лайк карточке
-router.delete('/:cardId/likes', dislikeCard); // убрать лайк с карточки
+
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().regex(/https?:\/\/(\w{3}\.)?[1-9a-z\-.]{1,}\w\w(\/[1-90a-z.,_@%&?+=~/-]{1,}\/?)?#?/),
+  }),
+}), createCard);
+
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().required().hex().length(24),
+  }),
+}), deleteCard);
+
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().required().hex().length(24),
+  }),
+}), likeCard); // поставить лайк карточке
+
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().required().hex().length(24),
+  }),
+}), dislikeCard); // убрать лайк с карточки
 
 module.exports = router;
